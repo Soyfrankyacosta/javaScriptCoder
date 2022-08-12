a48a7d6e466f24aaa643698bba7baa4ad08499d2
Francisco Acosta <franciscoivan.a.16@gmail.com>
2022-08-12 20:20:18 -0300

    agregado de array y funcion de eliminar tarea

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const fecha = document.querySelector('#fecha')
 const lista = document.querySelector('#lista')
+const input = document.querySelector('#input')
+const tareasGuardadas = []
+let id = 0
 
 let nombre = prompt('Ingrese su nombre y apellido:');
 document.getElementById('nombreUsuario').innerHTML = nombre;
+
 
 
 //Incorporando fecha actual
@@ -11,32 +15,65 @@ fecha.innerHTML = FECHA.toLocaleDateString('es', {weekday:'long', month:'short',
 
 //funcion para agregar una tarea nueva
 
-function agregarTareaNueva (tarea){
+function agregarTareaNueva (tarea,id,eliminada){
+    if(eliminada){return}
+
     const elemento = `  <li id="elemento">
                         <p class="texto-lista">${tarea}</p>
-                        <img class="circulo-lista" id="0" data="realizada" src="https://icongr.am/feather/circle.svg?size=40&color=5065b9" alt="circulo tarea terminada">
-                        <img class="eliminar-tarea" id="0" data="eliminada" src="https://icongr.am/feather/trash-2.svg?size=40&color=5065b9" alt="eliminar tarea">
+                        <img class="eliminar-tarea" id="${id}" data="eliminada" src="https://icongr.am/feather/trash-2.svg?size=40&color=5065b9" alt="eliminar tarea">
                         </li>
                     `
     lista.insertAdjacentHTML('beforeend', elemento)
 }
 
 enviar.addEventListener('click',()=>{
+    if (!input.value)
+    return alert('¡Por favor, asegurese de ingresar una tarea!')
     const tarea = input.value
     if(tarea) {
-        agregarTareaNueva(tarea)
+        agregarTareaNueva(tarea,id,false)
+        tareasGuardadas.push({
+            nombre:tarea,
+            id:id,
+            eliminada:false
+        })
     }
     input.value='' //una vez enviada la tarea el valor del input se restablece vacio
+    id++
 })
 
 //Agregar tarea presionando la tecla enter
 
 document.addEventListener('keyup',function(event){
+    if (!input.value)
+    return alert('¡Por favor, asegurese de ingresar una tarea!')
     if(event.key=='Enter'){
         const tarea = input.value
         if(tarea){
-            agregarTareaNueva(tarea)
+            agregarTareaNueva(tarea,id,false)
+        tareasGuardadas.push({
+            nombre:tarea,
+            id:id,
+            eliminada:false
+        })
         }
-        input.value='' 
+        input.value=''
+        id++
     }
 })
+
+
+lista.addEventListener('click',function(event){
+    const elemento = event.target
+    const elemntData = elemento.attributes.data.value
+    if(elemntData==='eliminada'){
+        tareaEliminada(elemento)
+    }
+})
+
+//funcion para eliminar una tarea
+
+function tareaEliminada(elemento){
+    elemento.parentNode.parentNode.removeChild(elemento.parentNode)
+    tareasGuardadas[elemento.id].eliminada = true
+}
