14694dbe63eeb00c894815b327b37941a52b79c8
Francisco Acosta <franciscoivan.a.16@gmail.com>
2022-08-23 18:16:37 -0300

    API y Fetch

--- js/script.js
+++ js/script.js
@@ -5,18 +5,76 @@ let tareasGuardadas
 let id 
 
 
+window.addEventListener('load', ()=>{
+    let lon
+    let lat
 
-//Sewwtalert para que el usuario ingrese nombre y apellido
-/*let nombre = Swal.fire(
-    {title: 'Bienvenido/a',
-    input: 'text',
-    inputLabel: 'Ingrese su nombre y apellido',
-    inputPlaceholder: 'nombre y apellido'},
-);
-document.getElementById('nombreUsuario').innerHTML = nombre;*/
+    let temperaturaValor = document.getElementById('temperaturaValor')
+    let temperaturaDescripcion = document.getElementById('temperaturaDescripcion')
 
-let nombre = prompt('Ingrese nombre y apellido:')
-document.getElementById('nombreUsuario').innerHTML = nombre;
+    let ubicacion = document.getElementById('ubicacionActual') 
+    let iconoAnimado = document.getElementById('ubicacionIcono')
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( posicion => {
+            lon = posicion.coords.longitude
+            lat = posicion.coords.latitude
+
+            //ubicación por ciudad tiempo actual
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=Escobar&lang=es&units=metric&appid=6fb3a49675c7e92d3bc3d21cb048953a`
+            
+            fetch(url)
+                .then(response => { return response.json() })
+                .then(data => {
+                    console.log(data.main.temp)
+                    let temp = Math.round(data.main.temp)
+                    temperaturaValor.textContent = `${temp} °C`
+                    let desc = data.weather[0].description
+                    temperaturaDescripcion.textContent = desc.toUpperCase()
+                    ubicacion.textContent = data.name
+                    //para iconos dinámicos
+                    console.log(data.weather[0].main)
+                    switch (data.weather[0].main) {
+                        case 'Thunderstorm':
+                            iconoAnimado.src = 'animated/thunder.svg'
+                            console.log('TORMENTA');
+                            break;
+                        case 'Drizzle':
+                            iconoAnimado.src = 'animated/rainy-2.svg'
+                            console.log('LLOVIZNA');
+                            break;
+                        case 'Rain':
+                            iconoAnimado.src = 'animated/rainy-7.svg'
+                            console.log('LLUVIA');
+                            break;
+                        case 'Snow':
+                            iconoAnimado.src = 'animated/snowy-6.svg'
+                            console.log('NIEVE');
+                            break;
+                        case 'Clear':
+                            iconoAnimado.src = 'animated/day.svg'
+                            console.log('LIMPIO');
+                            break;
+                        case 'Atmosphere':
+                            iconoAnimado.src = 'animated/weather.svg'
+                            console.log('ATMOSFERA');
+                            break;
+                        case 'Clouds':
+                            iconoAnimado.src = 'animated/cloudy-day-1.svg'
+                            console.log('NUBES');
+                            break;
+                        default:
+                            iconoAnimado.src = 'animated/cloudy-day-1.svg'
+                            console.log('por defecto');
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+    }
+})
+
 
 
 //Incorporando fecha actual
